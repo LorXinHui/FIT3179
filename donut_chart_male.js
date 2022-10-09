0680fccb44b515e0250a399dd1bc218e3c957d82
LorXinHui <114121595+LorXinHui@users.noreply.github.com>
2022-10-09 16:26:02 +0800

    Add files via upload

---
+++ donut_chart_male.js
@@ -0,0 +1,5 @@
+var vg_3 = "donut_chart_male.vg.json";
+
+vegaEmbed("#donut_chart_male", vg_3).then(function(result) {
+    //Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
