c32b3018572e951f85873007d0f9353ed1fae20c
LorXinHui <114121595+LorXinHui@users.noreply.github.com>
2022-10-13 07:37:31 +0800

    Add files via upload

---
+++ Visualization2/donut_chart_female.js
@@ -0,0 +1,5 @@
+var vg_2 = "donut_chart_female.vg.json";
+
+vegaEmbed("#donut_chart_female", vg_2).then(function(result) {
+    //Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
---
+++ Visualization2/map_services.js
@@ -0,0 +1,5 @@
+var vg_9 = "map_services.vg.json";
+
+vegaEmbed("#map_services", vg_9).then(function(result) {
+    //Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
