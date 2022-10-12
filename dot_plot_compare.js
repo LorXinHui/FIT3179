719affbef435191fbb470a39d3fac908d124ae36
LorXinHui <114121595+LorXinHui@users.noreply.github.com>
2022-10-13 07:43:09 +0800

    Add files via upload

---
+++ dot_plot_compare.js
@@ -0,0 +1,5 @@
+var vg_4 = "dot_plot_compare.vg.json";
+
+vegaEmbed("#dot_plot_compare", vg_4).then(function(result) {
+    //Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error)
---
+++ map_industry.js
@@ -0,0 +1,5 @@
+var vg_8 = "map_industry.vg.json";
+
+vegaEmbed("#map_industry", vg_8).then(function(result) {
+    //Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
